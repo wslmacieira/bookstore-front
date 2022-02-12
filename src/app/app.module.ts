048705505cd63dd2de3f96c1a7879dd5fe6333be
wslmacieira <wslmacieira@outlook.com>
2022-02-12 16:06:10 -0300

    categoria-read foi implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,6 +14,7 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { NavComponent } from './components/template/nav/nav.component';
+import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { HomeComponent } from './components/views/home/home.component';
 
 @NgModule({
@@ -21,6 +24,7 @@ import { HomeComponent } from './components/views/home/home.component';
     FooterComponent,
     NavComponent,
     HomeComponent,
+    CategoriaReadComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,6 +36,8 @@ import { HomeComponent } from './components/views/home/home.component';
     MatIconModule,
     MatCardModule,
     RouterModule,
+    MatTableModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoriaService } from '../categoria.service';
+import { Categoria } from './../categoria.model';
+
+@Component({
+  selector: 'app-categoria-read',
+  templateUrl: './categoria-read.component.html',
+  styleUrls: ['./categoria-read.component.css'],
+})
+export class CategoriaReadComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'nome', 'descricao', 'acoes'];
+  categorias: Categoria[] = [];
+  categorias$: Observable<Categoria[]> = this.service.findAll();
+
+  constructor(private service: CategoriaService) {}
+
+  ngOnInit(): void {
+    // this.findAll();
+  }
+
+  // findAll(): void {
+  //   this.service.findAll().subscribe((resposta) => {
+  //     this.categorias = resposta;
+  //   });
+  // }
+}
