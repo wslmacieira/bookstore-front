5c2a1a9e25c87d53603eaa60f3112bb624eeb8d8
wslmacieira <wslmacieira@outlook.com>
2022-02-12 18:53:53 -0300

    ajustes no categoria-read

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
@@ -38,6 +39,7 @@ import { HomeComponent } from './components/views/home/home.component';
     RouterModule,
     MatTableModule,
     HttpClientModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/views/categoria/categoria-read/categoria-read.component.ts
+++ src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -9,7 +9,7 @@ import { Categoria } from './../categoria.model';
   styleUrls: ['./categoria-read.component.css'],
 })
 export class CategoriaReadComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'nome', 'descricao', 'acoes'];
+  displayedColumns: string[] = ['id', 'nome', 'descricao', 'livros', 'acoes'];
   categorias: Categoria[] = [];
   categorias$: Observable<Categoria[]> = this.service.findAll();
 
